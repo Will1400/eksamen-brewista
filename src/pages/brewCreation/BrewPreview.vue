8e108b3665e8d30836d846f52d9ca2f28f0207d8
Will1400 <44838295+Will1400@users.noreply.github.com>
2021-01-12 13:20:54 +0100

    Added steps

--- src/pages/brewCreation/BrewPreview.vue
+++ src/pages/brewCreation/BrewPreview.vue
@@ -1,20 +1,28 @@
 <template>
 	<div class="flex items-start gap-4 mb-10 mt-4 h-full">
-		<button class="flex items-center justify-center">
+		<button @click="back" class="flex items-center justify-center">
 			<base-icon classes="w-6 h-6" icon="arrowBack"></base-icon>
 		</button>
 		<div>
-			<h1>{{ recipe.title }}</h1>
+			<h1 class="text-lg">{{ recipe.title }}</h1>
 			<recipe-small-details v-bind="recipe"></recipe-small-details>
 		</div>
 	</div>
 	<div>
-		<ul>
-			<li>step 1</li>
+		<ul class="flex flex-col gap-6">
+			<li v-for="(step, index) in recipe.steps" :key="index">
+				<p class="uppercase text-sm text-gray-500">
+					Step {{ index + 1 }}
+				</p>
+				<p class="text-gray-600">{{ step }}</p>
+			</li>
 		</ul>
 	</div>
-	<div>
-		<button @click="finishBrew">Finish</button>
+	<div class="flex justify-end mt-10">
+		<button @click="finishBrew" class="flex items-center gap-3">
+			<span class="uppercase font-bold">Finish</span>
+			<base-icon classes="w-5 h-5" icon="arrowForward"></base-icon>
+		</button>
 	</div>
 </template>
 
@@ -24,6 +32,9 @@ export default {
 	components: { RecipeSmallDetails },
 	props: ["coffeeId", "recipeId"],
 	methods: {
+		back() {
+			this.$router.go(-1);
+		},
 		finishBrew() {
 			this.$store.dispatch("brew/addBrew", {
 				recipe: this.recipe,
--- src/store/modules/recipes/index.js
+++ src/store/modules/recipes/index.js
@@ -13,6 +13,14 @@ export default {
 					waterAmount: "230ml",
 					grindSize: "Medium",
 					time: "1min",
+					steps: [
+						"Add ground coffee to inverse aeropress",
+						"Start timer",
+						"Add 50g of 93c water to aeropress for 10s",
+						"Stir five times",
+						"Add last 180g of water and stir to mix",
+						"After 1 min gently press coffee into cup",
+					],
 				},
 			],
 		};
